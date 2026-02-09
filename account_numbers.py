57ad4324196fd7fc53b33ca42dd4faffd7264ffb
Ben Cordero <bencord0@condi.me>
2026-02-09 13:31:25 +0000

    Update account_numbers script to diagnose errors

--- account_numbers.py
+++ account_numbers.py
@@ -14,11 +14,17 @@ def main():
     args = parser.parse_args()
 
     account_id = args.account_id
-    response = requests.get(f'https://api.octopus.energy/v1/accounts/{account_id}',
+    response = requests.get(f'https://api.octopus.energy/v1/accounts/{account_id}/',
         auth=(OCTOPUS_API_KEY, ''),
     )
 
-    print(json.dumps(response.json(), indent=2))
+    try:
+        data = response.json()
+    except Exception:
+        breakpoint()
+        raise
+
+    print(json.dumps(data, indent=2))
 
 
 if __name__ == '__main__':
