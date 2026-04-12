9e1786bc97802dd9edccabc91b71ddecc628a332
Ben Cordero (bencord0) <bencord0@condi.me>
2026-04-12 22:08:49 +0100

    set charging granularity to 8

--- queries/charging_slots.sql
+++ queries/charging_slots.sql
@@ -7,7 +7,7 @@
 -- 1.0 does not remove any slots, so don't go below that.
 -- 1.5 gives us longer candidates
 -- 2.0 to 4.0 gives us cheaper candidates, but smaller time windows
-.param set $GRANULARITY 4.0
+.param set $GRANULARITY 8.0
 
 -- Only inspect future time slots
 .param set $FROM_DATE strftime('%Y-%m-%dT%H:%M')
