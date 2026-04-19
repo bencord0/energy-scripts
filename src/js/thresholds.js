166e489ba098cd9a419ffa0c5790c4a985c0c970
Ben Cordero <bencord0@condi.me>
2026-04-19 23:22:12 +0100

    update thresholds: mask off flux tariffs while they are unavailable

--- src/js/thresholds.js
+++ src/js/thresholds.js
@@ -2,135 +2,135 @@
 export const importThresholds = [
     {
         name: "Intelligent Octopus Go",
-        value: 7.5,
+        value: 8,
         color: "#0077be",
     },
 
     {
         name: "Octopus Go",
-        value: 9,
+        value: 9.5,
         color: "#0077be",
     },
 
+    //{
+    //    name: "Snug Octopus",
+    //    value: 9,
+    //    color: "#0077be",
+    //},
+
     {
-        name: "Snug Octopus",
-        value: 9,
-        color: "#0077be",
+        name: "Flexible Octopus Night",
+        value: 12.94,
+        color: "#52be80",
     },
 
     {
         name: "Cosy Octopus",
-        value: 14.05,
+        value: 14.53,
         color: "#52be80",
     },
 
+    //{
+    //    name: "Octopus Flux",
+    //    value: 17.19,
+    //    color: "#f1c40f",
+    //},
+
+    //{
+    //    name: "Intelligent Octopus Flux",
+    //    value: 24.13,
+    //    color: "#e67e22",
+    //},
+
+    //{
+    //    name: "Octopus Flux Day",
+    //    value: 28.64,
+    //    color: "#e67e22",
+    //},
+
     {
-        name: "Flexible Octopus Night",
-        value: 14.64,
-        color: "#52be80",
+        name: "Flexible Octopus Day",
+        value: 30.84,
+        color: "#e74c3c",
     },
 
     {
-        name: "Octopus Flux",
-        value: 17.19,
-        color: "#f1c40f",
-    },
-
-    {
-        name: "Intelligent Octopus Flux",
-        value: 24.13,
-        color: "#e67e22",
-    },
-
-    {
-        name: "Octopus Flux Day",
-        value: 28.64,
-        color: "#e67e22",
-    },
-
-    {
-        name: "Octopush Cosy Day",
-        value: 28.65,
+        name: "Octopus Cosy Day",
+        value: 33.28,
         color: "#e67e22",
     },
 
     {
         name: "Intelligent Octopus Go Day",
-        value: 32.2,
+        value: 33.32,
         color: "#e74c3c",
     },
 
     {
         name: "Octopus Go Day",
-        value: 32.2,
+        value: 33.32,
         color: "#e74c3c",
     },
 
-    {
-        name: "Snug Octopus Day",
-        value: 31.65,
-        color: "#e74c3c",
-    },
+    //{
+    //    name: "Snug Octopus Day",
+    //    value: 31.65,
+    //    color: "#e74c3c",
+    //},
 
-    {
-        name: "Intelligent Octopus Flux Peak",
-        value: 32.17,
-        color: "#e74c3c",
-    },
+    //{
+    //    name: "Intelligent Octopus Flux Peak",
+    //    value: 32.17,
+    //    color: "#e74c3c",
+    //},
 
-    {
-        name: "Flexible Octopus Day",
-        value: 35.19,
-        color: "#e74c3c",
-    },
-
-    {
-        name: "Octopus Flux Peak",
-        value: 40.1,
-        color: "#e74c3c",
-    },
+    //{
+    //    name: "Octopus Flux Peak",
+    //    value: 40.1,
+    //    color: "#e74c3c",
+    //},
 
     {
         name: "Cosy Octopus Peak",
-        value: 42.97,
+        value: 51.68,
         color: "#e74c3c",
     },
 ]
 
 export const exportThresholds = [
-    {
-        name: "Octopus Flux",
-        value: 4.99,
-        color: "#52be80",
-    },
+    //{
+    //    name: "Octopus Flux",
+    //    value: 4.99,
+    //    color: "#52be80",
+    //},
 
-    {
-        name: "Octopus Flux Day",
-        value: 10.11,
-        color: "#52be80",
-    },
+    //{
+    //    name: "Octopus Flux Day",
+    //    value: 10.11,
+    //    color: "#52be80",
+    //},
 
     {
         name: "Outgoing Octopus",
-        value: 15,
+        value: 12,
         color: "#0077be",
     },
 
-    {
-        name: "Intelligent Octopus Flux",
-        value: 24.13,
-        color: "#00338a",
-    },
+    //{
+    //    name: "Intelligent Octopus Flux",
+    //    value: 24.13,
+    //    color: "#00338a",
+    //},
 
-    {
-        name: "Octopus Flux Peak",
-        value: 29.32,
-        color: "#00338a",
-    },
+    //{
+    //    name: "Octopus Flux Peak",
+    //    value: 29.32,
+    //    color: "#00338a",
+    //},
 
-    {
-        name: "Intelligent Octopus Flux Peak",
-        value: 32.17,
-        color: "#00338a",
-    },
+    //{
+    //    name: "Intelligent Octopus Flux Peak",
+    //    value: 32.17,
+    //    color: "#00338a",
+    //},
 ]
