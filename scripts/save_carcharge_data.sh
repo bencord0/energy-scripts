69fa23478079a9b012f48c9d6ee090a73f7cdebe
Ben Cordero <bencord0@condi.me>
2026-06-16 18:11:25 +0100

    Widen carcharge refresh window

--- scripts/save_carcharge_data.sh
+++ scripts/save_carcharge_data.sh
@@ -5,6 +5,8 @@ cargo run --bin save_carcharge_data -- \
     --username "${OHME_USERNAME}" \
     --password "${OHME_PASSWORD}" \
     --user-id  "${OHME_USER_ID}" \
+    --from     "$(date -d yesterday '+%Y-%m-%d')" \
+    --to       "$(date -d tomorrow '+%Y-%m-%d')" \
     --login --refresh \
     data/power.sqlite3
 
