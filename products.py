1a85db7316a5de12bc1c3336d718f31b6fd57c19
Ben Cordero <bencord0@condi.me>
2026-08-17 10:39:42 +0100

    Update products.py to allow scanning historical tariff data

--- products.py
+++ products.py
@@ -7,16 +7,29 @@ from pprint import pprint
 
 parser = ArgumentParser()
 parser.add_argument('--product-code')
+parser.add_argument('--available-at')  # ISO8601
 
 
 def main():
     args = parser.parse_args()
     url = 'https://api.octopus.energy/v1/products'
+
+    # This is a public API, no authentication required
     if product_code := args.product_code:
         url += '/' + product_code
+        response = requests.get(url)
+        data = response.json()
 
-    # This is a public API
-    response = requests.get(url)
+        data_file = f'data/products-{product_code}.json'
+        with open(data_file, 'w') as f:
+            json.dump(data, f, indent=2)
+        print(f'Saved product to {data_file}')
+        return
+
+    params = {}
+    if available_at := args.available_at:
+        params['available_at'] = available_at
+    response = requests.get(url, params=params)
     print(json.dumps(response.json(), indent=2))
 
 
